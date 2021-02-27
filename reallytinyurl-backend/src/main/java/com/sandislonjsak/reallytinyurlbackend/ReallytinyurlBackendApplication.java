366a1d0040ed5fbdba5260cd8c773f9256ec4e46
Sandi Slonjšak <sandi.slonjsak@gmail.com>
2021-02-27 11:24:19 +0100

    (build): generate backend and frontend projects. - add docker compose to quickly spin up postgres for local development

---
+++ reallytinyurl-backend/src/main/java/com/sandislonjsak/reallytinyurlbackend/ReallytinyurlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sandislonjsak.reallytinyurlbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReallytinyurlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReallytinyurlBackendApplication.class, args);
+	}
+
+}
---
+++ reallytinyurl-backend/src/test/java/com/sandislonjsak/reallytinyurlbackend/ReallytinyurlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandislonjsak.reallytinyurlbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReallytinyurlBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
